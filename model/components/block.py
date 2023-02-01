dad3d0456e73542504b6eaa323effd5a25a01899
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-02-02 08:14:55 +1000

    Update block with DecoderBlock

--- model/components/block.py
+++ model/components/block.py
@@ -8,7 +8,7 @@ from .selfattention import MultiHeadAttention
 
 class Block(nn.Module):
 
-    def __init__(self, n_heads, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT):
+    def __init__(self, n_heads, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT, masked = True):
         super().__init__()
 
         self.n_heads = n_heads
@@ -16,7 +16,7 @@ class Block(nn.Module):
         self.n_embed = n_embed
         self.block_size = block_size
 
-        self.mha = MultiHeadAttention(n_heads, self.head_size, n_embed, block_size, dropout=dropout)
+        self.mha = MultiHeadAttention(n_heads, self.head_size, n_embed, block_size, dropout=dropout, masked=masked)
         self.ffwd = FeedForward(n_embed, dropout=dropout)
 
         self.ln1 = nn.LayerNorm(n_embed)
@@ -25,5 +25,32 @@ class Block(nn.Module):
     def forward(self, x):
         x = x + self.mha(self.ln1(x))
         x = x + self.ffwd(self.ln2(x))
+
+        return x
+
+
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, n_heads, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT):
+        super().__init__()
+
+        self.n_heads = n_heads
+        self.head_size = n_embed // n_heads
+        self.n_embed = n_embed
+        self.block_size = block_size
+
+        self.mha = MultiHeadAttention(n_heads, self.head_size, n_embed, block_size, dropout=dropout,masked=True)
+        self.mha2 = MultiHeadAttention(n_heads, self.head_size, n_embed, block_size, dropout=dropout, masked=False)
+        self.ffwd = FeedForward(n_embed, dropout=dropout)
+
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+        self.ln3 = nn.LayerNorm(n_embed)
+
+    def forward(self, x, encoder_output):
+        x = x + self.mha(self.ln1(x))
+        x = x + self.mha2(self.ln2(x+encoder_output))
+        x = x + self.ffwd(self.ln3(x))
 
         return x
