db1de44096f12a0e0b13381d7c90655afa604896
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-02-02 07:19:12 +1000

    Add ability to use non masked attention

--- model/components/selfattention.py
+++ model/components/selfattention.py
@@ -7,39 +7,47 @@ from ..defaults import DEFAULT_DROPOUT, DEFAULT_EMBED, DEFAULT_BLOCK_SIZE
 
 class Head(nn.Module):
 
-    def __init__(self, head_size, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT):
+    def __init__(self, head_size, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT, masked = True):
         super().__init__()
 
         self.head_size = head_size
         self.n_embed = n_embed
         self.block_size = block_size
 
+        self.masked = masked
+
         self.key = nn.Linear(n_embed, head_size, bias=False)
         self.query = nn.Linear(n_embed, head_size, bias=False)
         self.value = nn.Linear(n_embed, head_size, bias=False)
-
+        
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
-        k = self.key(x)                                             #(B, T, C)
-        q = self.query(x)                                           #(B, T, C)
+        k = self.key(x)                                                 #(B, T, C)
+        q = self.query(x)                                               #(B, T, C)
 
-        wei = k @ q.transpose(-2,-1) / (C ** 0.5)                   #(B, T, T)
-        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) #(B, T, T)
-        wei = F.softmax(wei, dim=-1)                                #(B, T, T) 
-        wei = self.dropout(wei)                                     #(B, T, T)
+        if self.masked:
+            wei = k @ q.transpose(-2,-1) / (C ** 0.5)                   #(B, T, T)
+            wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) #(B, T, T)
+            wei = F.softmax(wei, dim=-1)                                #(B, T, T) 
+            wei = self.dropout(wei)                                     #(B, T, T)
 
-        v = self.value(x)                                           #(B, T, C)
-        out = wei @ v                                               #(B, T, C)
+        else:
+            wei = k @ q.transpose(-2,-1) / (C ** 0.5)                   #(B, T, T)
+            wei = F.softmax(wei, dim=-1)                                #(B, T, T) 
+            wei = self.dropout(wei)                                     #(B, T, T)
+
+        v = self.value(x)                                               #(B, T, C)
+        out = wei @ v                                                   #(B, T, C)
 
         return out
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, n_heads, head_size, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT):
+    def __init__(self, n_heads, head_size, n_embed = DEFAULT_EMBED, block_size = DEFAULT_BLOCK_SIZE, dropout = DEFAULT_DROPOUT, masked = True):
         super().__init__()
 
         self.n_heads = n_heads
@@ -47,7 +55,7 @@ class MultiHeadAttention(nn.Module):
         self.n_embed = n_embed
         self.block_size = block_size
 
-        self.heads = nn.ModuleList([Head(head_size, n_embed, block_size, dropout) for _ in range(n_heads)])
+        self.heads = nn.ModuleList([Head(head_size, n_embed, block_size, dropout, masked) for _ in range(n_heads)])
         self.proj = nn.Linear(n_heads * head_size, n_embed)
         self.dropout = nn.Dropout(dropout)
 
